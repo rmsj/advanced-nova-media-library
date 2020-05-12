2bf474b96d357ba7093ab76c1ebccd498c8cdec6
Ronaldo <ronaldomuniz@gmail.com>
2020-05-12 13:47:07 +1200

    Add setting and handling of image content when cropping. This, when saving image to S3/minio, avoids problem with cors.

--- src/Fields/HandlesConversionsTrait.php
+++ src/Fields/HandlesConversionsTrait.php
@@ -31,11 +31,31 @@ trait HandlesConversionsTrait
     {
         return [
             // original needed several purposes like cropping
-            '__original__' => $media->getFullUrl(),
+            '__original__' => $this->imageContent($media->getFullUrl(), $media) ,
             'indexView' => $media->getFullUrl($this->meta['conversionOnIndexView'] ?? ''),
             'detailView' => $media->getFullUrl($this->meta['conversionOnDetailView'] ?? ''),
-            'form' => $media->getFullUrl($this->meta['conversionOnForm'] ?? ''),
+            'form' => $this->imageContent($media->getFullUrl($this->meta['conversionOnForm'] ?? ''), $media),
             'preview' => $media->getFullUrl($this->meta['conversionOnPreview'] ?? ''),
         ];
+    }
+
+    /**
+     * Avoids CORS problem, handle crop with image content.
+     *
+     * @param $url
+     * @param $media
+     * @return string
+     */
+    public function imageContent($url, $media)
+    {
+        if (!config('nova-media-library.crop-with-image-content')) {
+            return $url;
+        }
+
+        // Read image path, convert to base64 encoding
+        $imageData = base64_encode(file_get_contents($url));
+
+        // Format the image SRC:  data:{mime};base64,{data};
+        return 'data: '.$media->mime_type.';base64,'.$imageData;
     }
 }
--- src/config/nova-media-library.php
+++ src/config/nova-media-library.php
@@ -2,4 +2,9 @@
 
 return [
     'enable-existing-media' => false,
+
+    /**
+     * Handle cropping with image base64 content
+     */
+    'crop-with-image-content' => true,
 ];
